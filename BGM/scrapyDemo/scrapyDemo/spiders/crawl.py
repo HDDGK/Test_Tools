e6255286be7249aaf3e17a748d88404f1c93cedb
HDDGK <129591233+HDDGK@users.noreply.github.com>
2023-04-30 22:52:10 +0800

    【练习】0430 22:50 网络爬虫模块，学习请求响应等内容，初步认知，里面有很多细节没有深究，需要后续继续学习，待反复学习看根因

--- BGM/scrapyDemo/scrapyDemo/spiders/crawl.py
+++ BGM/scrapyDemo/scrapyDemo/spiders/crawl.py
@@ -7,7 +7,7 @@ class QuotesSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url,callback=self.parse)
     def parse(self, response, **kwargs):
-        page=response.url.split("/")[-2]
+        page=response.url_Get.split("/")[-2]
         fileName='quotes-%s.html' % page
         with open(fileName,'wb')as f:
             f.write(response.body)
