4e297d5381d5b3151b4c9ec2a6e089fab180f6d6
HDDGK <129591233+HDDGK@users.noreply.github.com>
2023-06-02 00:34:16 +0800

    新增案例：待定，空

---
+++ MOOC_Spider/MC_Spider_Scrapy.py
@@ -0,0 +1,11 @@
+import scrapy
+'''
+爬虫框架，实现爬虫功能的软件结构和操作功能组件
+'''
+
+'''
+东方财富网: http://quote.eastmoney.com/stocklist.html获取个股信息
+百度股票:https://gupiao.baidu.com/stock/
+单个股票: https://gupiao.baidu.com/stock/sz002439.html
+待定
+'''
